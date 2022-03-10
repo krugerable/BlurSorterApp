21eb16f2d90561d04e738741dc496b2d99c51416
Dean Kruger <krugerable@gmail.com>
2022-03-10 11:04:27 +0200

    staged

--- blur_check.py
+++ blur_check.py
@@ -5,7 +5,7 @@ import cv2
 import pathlib
 
 #Constants
-FOCUS_THRESHOLD = 80
+FOCUS_THRESHOLD = 20
 BLURRED_DIR = 'blurred'
 OK_DIR = 'ok'
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -22,22 +22,24 @@ except:
 # iterate over files in
 # that directory
 for filename in os.listdir(CURR_DIR):
-    if filename.endswith('.JPG'):
         f = os.path.join(CURR_DIR, filename)
-
-        cv_image = cv2.imread(f)
-
-        # Covert to grayscale
-        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-
-        # Compute the Laplacian of the image and then the focus
-        #     measure is simply the variance of the Laplacian
-        variance_of_laplacian = cv2.Laplacian(gray, cv2.CV_64F).var()
-
-        # If below threshold, it's blurry
-        if variance_of_laplacian < FOCUS_THRESHOLD:
-            shutil.move(f, BLURRED_DIR)
-            blur_count += 1
-        else:
-            shutil.move(f, OK_DIR)
+        if f.endswith('.JPG'):
+            
             print(f)
+
+            #cv_image = cv2.imread(f)
+
+            # Covert to grayscale
+            gray = cv2.cvtColor(cv2.imread(f), cv2.COLOR_BGR2GRAY)
+
+            # Compute the Laplacian of the image and then the focus
+            #     measure is simply the variance of the Laplacian
+            variance_of_laplacian = cv2.Laplacian(gray, cv2.CV_64F).var()
+
+            # If below threshold, it's blurry
+            if variance_of_laplacian < FOCUS_THRESHOLD:
+                shutil.move(f, BLURRED_DIR)
+                blur_count += 1
+            else:
+                shutil.move(f, OK_DIR)
+                print(f)
